aebf2d2700a7360255272584f0aecdfc38671787
Andre Le <andre@andrele.com>
2014-02-16 01:59:07 -0500

    Consolidated canvas.js

--- public/canvas.js
+++ public/canvas.js
@@ -1,3 +1,8 @@
+		    $j = jQuery.noConflict();
+
+			var socket = io.connect('/');
+			var gameDuration = 10;
+
 			var canvas_width = window.innerWidth;
 			var canvas_height = window.innerHeight;
 			var blueTeamShield = new Sprite('shield.svg', -10, 10, 20, 30);
@@ -414,6 +419,36 @@
 
 	    	// Run this when the page loads
 		    $j(document).ready(function () {
+		    	$j("#start").on("click", function () {
+					$j("#splash").fadeOut();
+					socket.emit('gameStart',true);
+					countDown(gameDuration,function(){ socket.emit('gameEnd', true) });
+					game.running = true;
+				});
+				
 		    	$j('body').append(myCanvas);
 		    	game.initialize();
 		    });
+
+
+
+
+
+	var countDown = function (counter, callback) {
+            var timer = setInterval(countItDown,1000);
+
+            // Decrement the displayed timer value on each 'tick'
+            function countItDown(){
+                counter -= 1;
+                console.log('time:'+counter+'\n');
+         
+                if( counter <= 0 ){
+                    console.log('Countdown Finished.');
+                    // Stop the timer and do the callback.
+                    clearInterval(timer);
+                    callback();
+                    return;
+                }
+            }
+
+    };
