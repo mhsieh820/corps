375f6e58e84109903a2975c598703454630e28e3
mhsieh820 <mhsieh820@gmail.com>
2014-02-16 02:19:49 -0500

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var sendgrid  = require('sendgrid')('rrmallya', 'corpsgame', {api: 'smtp', port: 465});
 var routes = require('./routes');
 var http = require('http');
-//var fs = require('fs');
+var fs = require('fs');
 var path = require('path');
 
 var app = express();
